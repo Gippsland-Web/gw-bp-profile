cf12f60a53c40f1b1654c5c7e9155ad6614ae30f
James Kidd <kklolk@gmail.com>
2017-03-08 13:00:49 +1100

    Removed hardcoded JS, reviews now load own JS properly on profile page Cleaned up code.

---
+++ profile-nav-bar.php
@@ -0,0 +1,7 @@
+<div class="item-list-tabs no-ajax" id="subnav" aria-label="<?php esc_attr_e( 'Member secondary navigation', 'buddypress' ); ?>" role="navigation">
+	<ul>
+		<ul>
+		<li id="public-personal-li" class="current selected"> 
+<a id="public"  href="/members/<?php echo bp_core_get_username(bp_displayed_user_id());?>">View</a></li><li id="edit-personal-li" class=""><a id="edit" href="/members/<?php echo bp_core_get_username(bp_displayed_user_id());?>/profilev/edit/">Edit</a></li><li id="change-avatar-personal-li"><a id="change-avatar" href="/members/<?php echo bp_core_get_username(bp_displayed_user_id());?>/profilev/change-avatar/">Change Profile Photo</a></li><li id="change-cover-image-personal-li"><a id="change-cover-image" href="/members/<?php echo bp_core_get_username(bp_displayed_user_id());?>/profilev/change-cover-image/">Change Cover Image</a></li>	</ul>
+	</ul>
+</div>
